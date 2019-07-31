fba6391e181fc8a5df5a2a54104ea5779c028ee5
memo-ink <guillermo@inkwonders.com>
2019-07-31 12:09:12 -0500

    Actualizacion 15

--- assets/js/menus.js
+++ assets/js/menus.js
@@ -447,12 +447,6 @@ function bg_item(val){
       document.getElementById("item3").style.backgroundImage = "url('assets/img/item3_off.png')";
       document.getElementById("item4").style.backgroundImage = "url('assets/img/item4_on.png')";
       break;
-      default:
-        document.getElementById("item1").style.backgroundImage = "url('assets/img/item1_off.png')";
-        document.getElementById("item2").style.backgroundImage = "url('assets/img/item2_off.png')";
-        document.getElementById("item3").style.backgroundImage = "url('assets/img/item3_off.png')";
-        document.getElementById("item4").style.backgroundImage = "url('assets/img/item4_off.png')";
-      break;
     }
   }else{
     document.getElementById("item1").style.backgroundImage = "url('assets/img/item1_off.png')";
